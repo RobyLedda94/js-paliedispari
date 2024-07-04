9528d2ff465b3ea1de1b2df2c2390359b99e1e50
RobyLedda94 <robyledda01@yahoo.it>
2024-07-04 15:36:07 +0200

    [UPD] base scaffolding

---
+++ pari e dispari/js/script.js
@@ -0,0 +1,5 @@
+// Pari e Dispari
+// L’utente inserisce prima  "pari" o "dispari" poi inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
