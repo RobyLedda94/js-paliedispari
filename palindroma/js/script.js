94b5e34b229d6c87aee501ab3695062137397aae
RobyLedda94 <robyledda01@yahoo.it>
2024-07-04 15:46:00 +0200

    [UPD] user prompt

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -1,3 +1,12 @@
 // Palidroma
 // Chiedere all’utente di inserire una parola.
 // Creare una funzione per capire se la parola inserita è palindroma
+
+
+// chiedo all'utente di inserire una parola
+
+const parolaUtente = prompt("Inserisci una parola");
+// console.log(parolaUtente);
+
+
+
